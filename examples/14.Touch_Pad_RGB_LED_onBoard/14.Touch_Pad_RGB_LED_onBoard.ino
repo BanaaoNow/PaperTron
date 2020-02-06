04aef26ccf60526238e4b61faa77fe38f70cd947
Banaao - A Makers' Playground <60715548+BanaaoNow@users.noreply.github.com>
2020-02-07 02:18:39 +0530

    Update 14.Touch_Pad_RGB_LED_onBoard.ino

--- examples/14.Touch_Pad_RGB_LED_onBoard/14.Touch_Pad_RGB_LED_onBoard.ino
+++ examples/14.Touch_Pad_RGB_LED_onBoard/14.Touch_Pad_RGB_LED_onBoard.ino
@@ -2,9 +2,13 @@
 PaperTron papertron;
 
 int touchPad = 2;
-int ValueR = 255;  
-int ValueG = 0;
-int ValueB = 0;  
+int ValueR1 = 255;  
+int ValueG1 = 0;
+int ValueB1 = 0;  
+
+int ValueR2 = 0;  
+int ValueG2 = 0;
+int ValueB2 = 0; 
 
 void setup() {
   papertron.Begin();
@@ -14,6 +18,8 @@ void setup() {
 
 void loop() {
   if(papertron.isHigh(touchPad)){
-      papertron.onBoardRGB(ValueR,ValueG,ValueB);
+      papertron.onBoardRGB(ValueR1,ValueG1,ValueB1);
+    }
+    else{papertron.onBoardRGB(ValueR2,ValueG2,ValueB2);
     }
 }
