a80bbc70c475ef8932b274a5fcbf82a6bf23ea71
Banaao - A Makers' Playground <60715548+BanaaoNow@users.noreply.github.com>
2020-02-22 04:16:54 +0530

    Update 10.Reed_Switch.ino

--- examples/10.Reed_Switch/10.Reed_Switch.ino
+++ examples/10.Reed_Switch/10.Reed_Switch.ino
@@ -5,7 +5,7 @@ int ReedPin=4;     //Pin number at which Reed sensor is connected
 int Pin = 9;       //Pin number at which LED is connected
 int Delay = 500;   //Delay time
 
-int ReedValue;      //Variable to store Reed sensor value
+String ReedValue;      //Variable to store Reed sensor value
 
 void setup() {
   papertron.Begin();
